132d3b97d1b9ac4e60929890c3ed4f47728d5a36
Lece <lece619@gmail.com>
2022-11-12 22:57:49 +0900

    refactor : 불필요한 주석 제거

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -9,16 +9,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-/*
-당첨 통계
----
-3개 일치 (5,000원) - 1개
-4개 일치 (50,000원) - 0개
-5개 일치 (1,500,000원) - 0개
-5개 일치, 보너스 볼 일치 (30,000,000원) - 0개
-6개 일치 (2,000,000,000원) - 0개
-총 수익률은 62.5%입니다.
-* */
 public class OutputView {
 
     private static final String PURCHASE_MESSAGE = "구입금액을 입력해 주세요.";
